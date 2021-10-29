8b560a109dc565f5092c64ca6332d52e424a6cd6
djelal.fida <djelal.fida@student.howest.be>
2021-10-29 20:49:39 +0200

    handling error messages from login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,13 +8,18 @@
 <section id="login">
 
     <h3>Sign In</h3>
+    @foreach ($errors->all() as $error)
+    <p>{{ $error }}</p>  
+    @endforeach
+
             
-            <form action="#">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
                 <label for="email">Email Address</label>
-                <input type="text" name="email" id="email">
+                <input type="text" name="email" id="email" required>
 
                 <label for="password">Password</label>
-                <input type="password" name="password" id="password">
+                <input type="password" name="password" id="password" required>
 
                 <input class="btn-primary" type="submit" value="Sign In">
             </form>
