de7d3004c2d8de79d0ad6bfde18afe6368fb9364
djelal.fida <djelal.fida@student.howest.be>
2021-11-04 15:48:03 +0100

    Adding a new car offer

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Car;
-use App\Models\Carmodel;
-use App\Models\Feature;
 use App\Models\Offer;
+use App\Models\Feature;
+use App\Models\Carmodel;
+use Illuminate\Http\Request;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+use Illuminate\Support\Facades\Auth;
 
 class CarController extends Controller
 {
@@ -31,6 +33,51 @@ class CarController extends Controller
     }
 
     function addCar(Request $request) {
-        dd($request->input("features"));
+        $results = $this -> validateOffer($request);
+
+        $offer = new Offer;
+
+        $offer -> featureslist = json_encode($results["features"]);
+        $offer -> modelName = $results["model"];
+        $offer -> typeModel = $results["type"];
+        $offer -> price = $results["price"];
+        $offer -> odometer = $results["odometer"];
+        $offer -> accel = $results["accel"];
+        $offer -> range = $results["range"];
+        $offer -> topSpeed = $results["topspeed"];
+
+        $imagesUrl = [];
+
+        foreach ($results["carimages"] as $item) {
+            $url = Cloudinary::upload($item->getRealPath())->getSecurePath();
+            array_push($imagesUrl, $url);
+        }
+
+        $offer -> images = json_encode($imagesUrl);
+        $offer -> year = $results["year"];
+        $offer -> seller = Auth::user() -> id;
+
+        $offer -> save();
+
+        return redirect()->back()->with("message", "Profile successfully updated!");
     }
+
+    function validateOffer(Request $request) {
+        $rules = [
+            "model" => "required|string|min:5|max:7",
+            "type" => "required|string|min:5|max:27",
+            "year" => "required|integer",
+            "features" => "required|array",
+            "price" => "required|integer",
+            "odometer" => "required|integer",
+            "accel" => "required|integer",
+            "range" => "required|integer",
+            "topspeed" => "required|integer",
+            "carimages" => 'required|array',
+            "carimages.*" => 'image|mimes:jpg,jpeg,png'
+        ];
+
+        return $request -> validate($rules);
+    }
+
 }
--- public/assets/js/car.js
+++ public/assets/js/car.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", init);
 let currentIndex = 0;
-const images = []
+const images = [];
 
 //images.push("https://static-assets.tesla.com/configurator/compositor?context=design_studio_2&options=$BP02,$HC00,$ADPX2,$GLFR,$AU00,$APF2,$APH3,$APPF,$X028,$BTX5,$BS00,$BCMB,$CH05,$CW00,$COUS,$X040,$IDBA,$X027,$DRLH,$DU00,$AF00,$FMP6,$FG02,$DCF0,$FR04,$TD00,$X007,$X011,$INBTB,$PI00,$IX00,$X001,$LP01,$LT1B,$MI03,$X037,$MDLS,$DV4W,$X025,$X003,$ZCST,$PBSB,$PS01,$PK00,$X031,$PX00,$PF00,$X043,$TM00,$BR04,$RENA,$RFP2,$USSB,$X014,$ME02,$QTTB,$SR07,$SP00,$X021,$SC04,$SU01,$TR00,$TIG4,$DSHG,$MT75A,$UTPB,$WTAS,$WR01,$YFCC,$CPF0&view=STUD_SEAT_ALTA&model=ms&size=1441&bkba_opt=2&version=v0028d202110280433&crop=0,0,0,0&version=v0028d202110280433");
 //images.push("https://static-assets.tesla.com/configurator/compositor?context=design_studio_2&options=$BP02,$HC00,$ADPX2,$GLFR,$AU00,$APF2,$APH3,$APPF,$X028,$BTX5,$BS00,$BCMB,$CH05,$CW00,$COUS,$X040,$IDBA,$X027,$DRLH,$DU00,$AF00,$FMP6,$FG02,$DCF0,$FR04,$TD00,$X007,$X011,$INBTB,$PI00,$IX00,$X001,$LP01,$LT1B,$MI03,$X037,$MDLS,$DV4W,$X025,$X003,$ZCST,$PBSB,$PS01,$PK00,$X031,$PX00,$PF00,$X043,$TM00,$BR04,$RENA,$RFP2,$USSB,$X014,$ME02,$QTTB,$SR07,$SP00,$X021,$SC04,$SU01,$TR00,$TIG4,$DSHG,$MT75A,$UTPB,$WTAS,$WR01,$YFCC,$CPF0&view=STUD_SIDE&model=ms&size=1441&bkba_opt=2&version=v0028d202110280433&crop=0,0,0,0&version=v0028d202110280433");
@@ -25,7 +25,7 @@ function init() {
         }
 
       }, false);
-
+    
     function removeScrollArrow(e) {
         e.preventDefault();
         const $el = document.querySelector("#car-information");
