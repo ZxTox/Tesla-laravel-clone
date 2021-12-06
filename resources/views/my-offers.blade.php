4f518524cbc2b651a2c0625c63220effb30fdddc
djelal.fida <djelal.fida@student.howest.be>
2021-12-06 16:22:10 +0100

    styling edit my offer menu

--- resources/views/my-offers.blade.php
+++ resources/views/my-offers.blade.php
@@ -8,7 +8,12 @@
 @include("layouts.header")
 
 <div id="container">
-    
+
+    <div class="menu-overlay">
+    <section id="edit-menu">
+        
+    </section>
+</div>
 
     <section id="my-offers">
         <h2>My Offers</h2>
@@ -19,9 +24,21 @@
 
                 <h3>{{ $offer -> modelName }}</h3>
                 <h4>{{ $offer -> typeModel }}</h4>
-                <a class="btn-primary" href="{{ route("car", $offer -> offerid) }}">
-                    View Offer
-                </a>
+                <p>{{ $offer -> price }}</p>
+                <div>
+                    <a href="{{ route("car", $offer -> offerid) }}">
+                        <button class="btn-secondary">
+                            View Offer
+                        </button>  
+                        </a>
+
+                    <a href="{{ route("car", $offer -> offerid) }}">
+                    <button class="btn-primary">
+                        Edit Offer
+                    </button>  
+                    </a>
+                </div>
+                
                 
             </div>
         @endforeach
